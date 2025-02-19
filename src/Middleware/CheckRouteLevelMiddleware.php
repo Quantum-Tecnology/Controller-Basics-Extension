eb8244b2fdb5269809e59e3a1afbcddd53b6061a
luis.santarosa@mobiis.com.br <bolota_xd@hotmail.com>
2025-02-18 21:52:46 -0300

    Create CheckRouteLevelMiddleware.php

---
+++ src/Middleware/CheckRouteLevelMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GustavoSantarosa\ControllerBasicsExtension\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CheckRouteLevelMiddleware
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, \Closure $next, $level)
+    {
+        abort_unless(
+            auth()->check(),
+            Response::HTTP_UNAUTHORIZED,
+            __('Unauthorized')
+        );
+
+        $canPass = match (auth()->level()) {
+            'beta'  => ['beta'],
+            'alpha' => ['alpha', 'beta'],
+            'test'  => ['test', 'alpha', 'beta'],
+            default => [],
+        };
+
+        abort_unless(
+            in_array($level, $canPass),
+            Response::HTTP_FORBIDDEN,
+            __('Access denied')
+        );
+
+        return $next($request);
+    }
+}
