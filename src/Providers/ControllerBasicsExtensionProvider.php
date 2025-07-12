bb26726ca81eec09d893df54785f49ff7f05bc8e
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-07-12 07:32:12 -0300

    feat: add configuration files and middleware integration in ControllerBasicsExtensionProvider

--- src/Providers/ControllerBasicsExtensionProvider.php
+++ src/Providers/ControllerBasicsExtensionProvider.php
@@ -4,9 +4,10 @@ declare(strict_types = 1);
 
 namespace QuantumTecnology\ControllerBasicsExtension\Providers;
 
-use Illuminate\Routing\Router;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\ServiceProvider;
 use Override;
+use QuantumTecnology\ControllerBasicsExtension\Middleware\LogMiddleware;
 
 final class ControllerBasicsExtensionProvider extends ServiceProvider
 {
@@ -16,20 +17,24 @@ final class ControllerBasicsExtensionProvider extends ServiceProvider
     #[Override]
     public function register(): void
     {
+        foreach ([
+            'hashids' => __DIR__ . '/../Config/hashids.php',
+            'bind'    => __DIR__ . '/../Config/bind.php',
+            'page'    => __DIR__ . '/../Config/page.php',
+        ] as $key => $path) {
+            $this->mergeConfigFrom($path, $key);
+        }
     }
 
-    /**
-     * Bootstrap services.
-     */
-    public function boot(Router $router): void
+    public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/../config/hashids.php' => config_path('hashids.php'),
+            __DIR__ . '/../config/hashids.php' => __DIR__ . '/../hashids.php',
+            __DIR__ . '/../config/bind.php'    => __DIR__ . '/../bind.php',
+            __DIR__ . '/../config/page.php'    => __DIR__ . '/../page.php',
         ], 'config');
 
-        /*
-         * TODO: esta apresentnado erro ao tentar registrar o provider
-         */
-        // $router->setRegistrar(new EnhancedResourceProvider($router));
+        $this->app->make(Kernel::class)
+            ->pushMiddleware(LogMiddleware::class);
     }
 }
