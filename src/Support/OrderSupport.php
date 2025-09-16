ef1020baec408e3226b0d36eed3f927c44cd7df2
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-16 13:45:38 -0300

    feat: add OrderSupport class with initial parse method

---
+++ src/Support/OrderSupport.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace QuantumTecnology\ControllerBasicsExtension\Support;
+
+class OrderSupport
+{
+    public function parse(array $data): array
+    {
+        return [];
+    }
+}
