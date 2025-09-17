161777aaf039f5a1c58781f5d72ab03015999ab5
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-17 15:52:19 -0300

    feat: add logging for perPage value exceeding maximum limit in Pagination

--- src/Builder/Request/Pagination.php
+++ src/Builder/Request/Pagination.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace QuantumTecnology\ControllerBasicsExtension\Builder\Request;
 
+use QuantumTecnology\ControllerBasicsExtension\Support\LogSupport;
+
 class Pagination
 {
     protected array $data = [];
@@ -15,6 +17,18 @@ class Pagination
 
     public function add(string $field, int $limit, int $perPage): void
     {
+        if ($perPage > config('page.max_page')) {
+            LogSupport::add(
+                __('The :field value (:per_page) exceeds the maximum allowed (:max_page). It has been set to the maximum value of :max_page.', [
+                    'per_page' => $perPage,
+                    'max_page' => config('page.max_page'),
+                    'field'    => 'per_page_' . str_replace('.', '_', $field),
+                ])
+            );
+
+            $perPage = config('page.max_page');
+        }
+
         $this->data[] = (object) [
             'field'    => $field,
             'limit'    => $limit,
