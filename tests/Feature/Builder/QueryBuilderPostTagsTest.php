6111c3a141cb1f0fdeb0db985ae6e811de8acc44
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-18 09:47:36 -0300

    test: add tests for QueryBuilder to validate post tags and tag names retrieval

--- tests/Feature/Builder/QueryBuilderPostTagsTest.php
+++ tests/Feature/Builder/QueryBuilderPostTagsTest.php
@@ -9,12 +9,22 @@ use QuantumTecnology\ControllerBasicsExtension\Tests\Fixtures\App\Models\Tag;
 beforeEach(fn () => $this->builder = app(QueryBuilder::class));
 
 test('it returns post with tags and tags count', function () {
-    $tags = Tag::factory(20)->create();
+    $tags = Tag::factory(20)->create(['name' => 'test']);
     $post = Post::factory()->create();
     $post->tags()->attach($tags);
 
-    $response = $this->builder->execute(new Post(), 'id tags')->sole();
+    $response = $this->builder->execute(new Post(), 'id tags { name }')->sole();
 
     expect($response->tags_count)->toBe(20)
         ->and($response->tags->count())->toBe(10);
+});
+
+test('it returns post with attached tag name', function () {
+    $tags = Tag::factory()->create(['name' => 'test from tag']);
+    $post = Post::factory()->create();
+    $post->tags()->attach($tags);
+
+    $response = $this->builder->execute(new Post(), 'id tags { name }')->sole();
+
+    expect($response->tags->first()->name)->toBe('test from tag');
 });
