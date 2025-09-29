ec6693a66fd578c3ae38ec43b1d4176a0470f2f0
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-29 16:08:10 -0300

    feat: add update method to AsGraphQLController for handling model updates with field filtering

--- src/Traits/AsGraphQLController.php
+++ src/Traits/AsGraphQLController.php
@@ -7,8 +7,10 @@ namespace QuantumTecnology\ControllerBasicsExtension\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use QuantumTecnology\ControllerBasicsExtension\Builder\GraphBuilder;
 use QuantumTecnology\ControllerBasicsExtension\Builder\QueryBuilder;
+use QuantumTecnology\ControllerBasicsExtension\Services\RelationshipService;
 
 trait AsGraphQLController
 {
@@ -37,6 +39,21 @@ trait AsGraphQLController
         ]);
     }
 
+    public function update(GraphBuilder $graphBuilder, Request $request): JsonResponse
+    {
+        $dataValues = $this->getDataRequest('update');
+
+        $fields = request()->query('fields');
+
+        $model = $this->findBy($fields);
+
+        $modelUpdate = $this->execute($model, $dataValues);
+
+        return response()->json([
+            'data' => $graphBuilder->execute($modelUpdate, fields: $fields, onlyFields: $this->allowedFields(), options: $request->query()),
+        ]);
+    }
+
     protected function allowedFields(): ?array
     {
         return null;
@@ -53,7 +70,61 @@ trait AsGraphQLController
             $fields,
             [
                 "filter_({$keyName})" => $idFromParam,
-            ] + request()->query(),
+            ] + $this->filterRouteParams($routeParams) + request()->query(),
         )->sole();
+    }
+
+    protected function filterRouteParams(array $data): array
+    {
+        return collect($data)
+            ->mapWithKeys(fn ($value, $key) => ['filter_(' . $key . ')' => $value])
+            ->all();
+    }
+
+    /** @codeCoverageIgnore */
+    protected function getDataRequest(?string $action = null, bool $exact = false): array
+    {
+        $class = static::class;
+        $parts = explode('Controller', $class);
+
+        if (count($parts) > 2) {
+            $last        = array_pop($parts);
+            $penultimate = array_pop($parts);
+            $class       = implode('Controller', $parts) . 'Request' . $penultimate . 'Request' . $last;
+        } else {
+            $class = str_replace('Controller', 'Request', $class);
+        }
+        $class = str_replace('\\Controller\\', '\\Request\\', $class);
+
+        $value = str_replace(['App\\Http\\Controllers\\'],
+            ['App\\Http\\Requests\\'],
+            static::class);
+
+        if (blank($action)) {
+            $request = app($class);
+            $request->validated();
+
+            return $request->validated();
+        }
+
+        $value = mb_substr($value, 0, -7) . '\\' . ucfirst($action) . 'Request';
+
+        if (class_exists($value)) {
+            $request = app($value);
+            $request->validated();
+
+            return $request->validated();
+        }
+
+        if ($exact) {
+            return [];
+        }
+
+        return self::getDataRequest();
+    }
+
+    protected function execute(Model $model, array $data)
+    {
+        return DB::transaction(fn () => app(RelationshipService::class)->execute($model, $data));
     }
 }
