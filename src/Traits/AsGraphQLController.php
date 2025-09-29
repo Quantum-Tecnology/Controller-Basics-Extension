55937a1d5c9f132880b1afd08e62cb826c775314
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-09-29 15:01:45 -0300

    feat: enhance index and show methods in AsGraphQLController with pagination and field filtering

--- src/Traits/AsGraphQLController.php
+++ src/Traits/AsGraphQLController.php
@@ -4,8 +4,10 @@ declare(strict_types = 1);
 
 namespace QuantumTecnology\ControllerBasicsExtension\Traits;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use QuantumTecnology\ControllerBasicsExtension\Builder\GraphBuilder;
 use QuantumTecnology\ControllerBasicsExtension\Builder\QueryBuilder;
 
@@ -13,10 +15,43 @@ trait AsGraphQLController
 {
     abstract protected function model(): Model;
 
-    public function index(QueryBuilder $queryBuilder, GraphBuilder $graphBuilder, Request $request)
+    public function index(QueryBuilder $queryBuilder, GraphBuilder $graphBuilder, Request $request): Collection
     {
-        $query = $queryBuilder->execute($this->model(), $request->query('fields'))->simplePaginate();
+        $result = $queryBuilder->execute($this->model(), $request->query('fields'), $request->query());
 
-        return $graphBuilder->execute($query, $request->query('fields'));
+        if ($request->query('order_column')) {
+            $result->orderBy($request->query('order_column'), $request->query('order_direction'));
+        }
+
+        $query = $result->simplePaginate();
+
+        return $graphBuilder->execute($query, $request->query('fields'), $this->allowedFields());
+    }
+
+    public function show(GraphBuilder $graphBuilder, Request $request): Collection
+    {
+        $response = $this->findBy($request->query('fields'))->sole();
+
+        return $graphBuilder->execute($response, $request->query('fields'), $request->query());
+    }
+
+    protected function allowedFields(): ?array
+    {
+        return null;
+    }
+
+    protected function findBy(array $fields): Builder
+    {
+        $routeParams = request()->route()?->parameters() ?: [];
+        $idFromParam = array_pop($routeParams);
+        $keyName     = $this->model()->getKeyName();
+
+        return app(QueryBuilder::class)->execute(
+            $this->model(),
+            $fields,
+            [
+                "filter_{$keyName}" => $idFromParam,
+            ] + request()->query(),
+        );
     }
 }
