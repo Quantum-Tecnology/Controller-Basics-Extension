b5fdd2077f0f93629c45322cc5e25fc05970de2b
Bruno Costa <bhcosta90@gmail.com>
2025-11-11 08:49:04 -0300

    Add ByFilterTrait for flexible filtering of Eloquent models

---
+++ src/Models/ByFilterTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace QuantumTecnology\ControllerBasicsExtension\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+
+trait ByFilterTrait
+{
+    #[Scope]
+    public function byFilter(Builder $builder, Collection $fields, Collection $values): void
+    {
+        $builder->where(function ($builder) use ($fields, $values) {
+            foreach ($values as $value) {
+                foreach ($fields as $field) {
+                    $builder->orWhereLike($field, '%' . $value . '%');
+                }
+            }
+        });
+    }
+}
