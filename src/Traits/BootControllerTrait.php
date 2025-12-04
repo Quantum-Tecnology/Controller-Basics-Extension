a0ea4a73c76e2ef542c5dc9772f4c3e4266082d7
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-12-04 15:12:12 -0300

    Call booted() when defaultService is string
    Added a check in getService() to call booted() if defaultService is a string. This ensures proper initialization when the service is not yet instantiated.

--- src/Traits/BootControllerTrait.php
+++ src/Traits/BootControllerTrait.php
@@ -63,6 +63,10 @@ trait BootControllerTrait
 
     public function getService(): ServiceInterface | string
     {
+        if(is_string($this->defaultService)) {
+            $this->booted();
+        }
+
         return $this->defaultService;
     }
 
